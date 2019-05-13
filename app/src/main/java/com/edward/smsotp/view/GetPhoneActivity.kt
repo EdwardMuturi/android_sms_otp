bb0c7c3d919ccaf1a60020301e38286dc075b9e4
EdwardMuturi <kakaturi300@gmail.com>
2019-05-14 00:43:14 +0300

    rename main activity layout to activity_get_phone

--- app/src/main/java/com/edward/smsotp/view/GetPhoneActivity.kt
+++ app/src/main/java/com/edward/smsotp/view/GetPhoneActivity.kt
@@ -1,16 +1,16 @@
 package com.edward.smsotp.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.edward.smsotp.R
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_get_phone.*
 
 class GetPhoneActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_get_phone)
 
         ccp_next_btn.setOnClickListener{
             val smsIntent= Intent(this@GetPhoneActivity, VerifySmsActivity::class.java)
